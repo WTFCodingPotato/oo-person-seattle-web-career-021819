7c2d09b9b8040c814f023d8a124cd86b06d47f53
Joshua M Loran <joshua.loran@gmail.com>
2019-02-21 22:18:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,32 +1,18 @@
 class Person
-<<<<<<< HEAD
   attr_accessor :bank_account
   attr_reader :happiness, :hygiene
 
-=======
-  attr_accessor :bank_account, :hygiene
-  attr_reader :happiness
-  
->>>>>>> 1526f426f529ca4166b2552ee8aaf12b363a0ee4
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
   end
-<<<<<<< HEAD
 
   def name
     @name
   end
 
-=======
-  
-  def name
-    @name
-  end
-  
->>>>>>> 1526f426f529ca4166b2552ee8aaf12b363a0ee4
   def happiness=(happy_index)
     if happy_index > 9
       @happiness = 10
@@ -36,13 +22,8 @@ class Person
       @happiness = happy_index
     end
   end
-<<<<<<< HEAD
 
   def hygiene=(hygiene_index)
-=======
-  
-    def hygiene=(hygiene_index)
->>>>>>> 1526f426f529ca4166b2552ee8aaf12b363a0ee4
     if hygiene_index > 9
       @hygiene = 10
     elsif hygiene_index < 0
@@ -51,38 +32,25 @@ class Person
       @hygiene = hygiene_index
     end
   end
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> 1526f426f529ca4166b2552ee8aaf12b363a0ee4
   def happy?
     if @happiness > 7
       return true
     end
     false
   end
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> 1526f426f529ca4166b2552ee8aaf12b363a0ee4
   def clean?
     if @hygiene > 7
       return true
     end
     false
   end
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> 1526f426f529ca4166b2552ee8aaf12b363a0ee4
   def get_paid(salary)
     @bank_account += salary
     "all about the benjamins"
   end
-<<<<<<< HEAD
 
   def take_bath
     self.hygiene += 4
@@ -117,15 +85,3 @@ class Person
 
 
 end
-=======
-  
-  def take_bath
-    @hygiene += 4
-    hygiene
-    "♪ Rub-a-dub just relaxing in the tub ♫"
-  end
-    
-  
-  
-end
->>>>>>> 1526f426f529ca4166b2552ee8aaf12b363a0ee4
